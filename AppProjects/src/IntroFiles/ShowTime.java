c9285a0db1896d6f55a9f4fc110ea24fac3e90f0
Tyson Miller <tyson.miller@tooeleschools.org>
2019-08-30 09:32:41 -0600

    ShowTime Activity

---
+++ AppProjects/src/IntroFiles/ShowTime.java
@@ -0,0 +1,16 @@
+/*
+Tyson Miller
+30 August 2019
+Mobile app dev
+*/
+
+import java.util.Calendar;
+
+
+class Showtime
+{
+	public static void main(String[] args)
+	{
+		System.out.println(Calendar.getInstance().getTime());
+	}
+}
